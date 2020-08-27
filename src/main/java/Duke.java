53fe68586f878c6692ad2fd9accf5b15d278a80c
chocomango <weilingchan97@gmail.com>
2020-08-27 20:48:33 +0800

    Level 3
    Add the ability to mark tasks as done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,32 @@
 import java.util.Scanner;
+import java.util.regex.*;
 
 public class Duke {
-    private static String[] disk = new String[100];
-    private static int sizeofDisk = 0;
+    private static Task[] tasks = new Task[100];
+    private static int taskCount = 0;
 
     //prints all the stored text from user input
-    public static void printDisk() {
-        System.out.println("You said too much.");
-        for (int i = 0; i < sizeofDisk; i++) {
-            System.out.printf("%d. %s%n", i+1, disk[i]);
+    public static void printTasks() {
+        System.out.println("You said so much:");
+        for (int i = 0; i < taskCount; i++) {
+            System.out.printf("%d. %s%n", i+1, tasks[i]);
+        }
+    }
+
+	//add new task to tasks
+	public static void addTask(String description) {
+        tasks[taskCount++] = new Task(description);
+        System.out.println("Got it: " + description);
+    }
+
+    //set task to be completed
+    public static void completeTask(int index) {
+        index--;
+        if (index >= 0 && index < taskCount) {
+            tasks[index].setStatus(true);
+            System.out.println("Getting rid of this:" + "\n\t" + tasks[index].toString());
+        } else {
+            System.out.println("You never said this.");
         }
     }
 
@@ -29,15 +47,22 @@ public class Duke {
         System.out.println(logo);
         System.out.println("What can I do for you?\n");
         Scanner in = new Scanner(System.in);
-        String input = in.nextLine();
+
+        String input = in.nextLine().trim();
+		//Regex to find done command
+		String regExp = "^done \\d$";
+        Pattern pattern = Pattern.compile(regExp);
+
         while (!input.equals("bye")) {
             if ("list".equals(input)) {
-                printDisk();
-            } else {
-                disk[sizeofDisk++] = input;
-                System.out.println("You said: " + input);
+                printTasks();
+            } else if(pattern.matcher(input).find()){
+			    int selectIndex = Integer.parseInt(input.substring(input.indexOf(' ') + 1));
+				completeTask(selectIndex);
+			} else {
+                addTask(input);
             }
-            input = in.nextLine();
+            input = in.nextLine().trim();
         }
         System.out.println("Until Next Time...");
     }
