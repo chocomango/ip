227bee4cd041d1a6f0f6c5cd0eae04a6bdaebd2a
chocomango <weilingchan97@gmail.com>
2020-08-27 16:35:08 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner in = new Scanner(System.in);
+        String input;
+        String logo =
+                 "   ....,       ,....\n"
+                +" .' ,,, '.   .' ,,, '.\n"
+                +"  .`   `.     .`   `.\n"
+                +" : ..... :   : ..... :\n"
+                +" :`~'-'-`:   :`-'-'~`:\n"
+                +"  `.~-`.'     `.~`'.'\n"
+                +"    ```   ___   ```\n"
+                +"        ( . . )\n\n"
+                +"         .._..\n"
+                +"       .'     '.\n"
+                +"      `.~~~~~~~.`\n"
+                +"        `-...-`\n";
+        System.out.println(logo);
+        System.out.println("What can I do for you?\n");
+        input = in.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = in.nextLine();
+        }
+        System.out.println("Until Next Time...");
     }
 }
